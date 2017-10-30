b8223f3a98c4a22989ed1f975b5bbac6282824e0
Draker2k2 <33083335+Draker2k2@users.noreply.github.com>
2017-10-30 13:14:15 +0100

    fixing defenses reducer actions

--- src/reducers/defenses.js
+++ src/reducers/defenses.js
@@ -6,6 +6,39 @@ const INITIAL_STATE = {
   error: false,
 };
 
+export function recalculateTotalBonus(state) {
+  state.defenses.map(defense => defense.totalBonus = 0);
+  state.defenses.filter(defense => defense.id !== 'AC').map(defense => defense.classModifiers.map(classModifier =>
+    defense.totalBonus += +classModifier.value,
+  ));
+  state.defenses.map(defense => defense.bonuses.map(bonus =>
+    defense.totalBonus += +bonus.value,
+  ));
+}
+
+
+function increaseBonusXX(defenseToIncrement, type, value) {
+  if (type === 'bonus') {
+    defenseToIncrement.bonuses.filter(bonus => (bonus.type === 'bonus' ? bonus.value += value : false));
+  } else if (type === 'dodge') {
+    defenseToIncrement.bonuses.filter(bonus => (bonus.type === 'dodge' ? bonus.value += value : false));
+  } else {
+    defenseToIncrement.bonuses
+      .filter(bonus => (bonus.type === type && bonus.value < Math.abs(value) ? bonus.value = value : 0));
+  }
+}
+
+function decreaseBonusXX(defenseToIncrement, type, value) {
+  if (type === 'bonus') {
+    defenseToIncrement.bonuses.filter(bonus => (bonus.type === 'bonus' ? bonus.value -= value : false));
+  } else if (type === 'dodge') {
+    defenseToIncrement.bonuses.filter(bonus => (bonus.type === 'dodge' ? bonus.value -= value : false));
+  } else {
+    defenseToIncrement.bonuses
+      .filter(bonus => (bonus.type === type && bonus.value === value ? bonus.value -= value : 0));
+  }
+}
+
 export default function (state = INITIAL_STATE, action) {
   switch (action.type) {
     case ACTION_TYPES.FETCH_DATA:
@@ -20,13 +53,23 @@ export default function (state = INITIAL_STATE, action) {
       const defenseToIncrement = state.defenses
         .filter(item => item.id === action.payload.defenseName)[0];
       if (action.payload.checked === true) {
-        defenseToIncrement.inherentValue -= action.payload.value;
+        decreaseBonusXX(defenseToIncrement, action.payload.bonusType, action.payload.value);
+        // defenseToIncrement.inherentValue -= action.payload.value;
       } else {
-        defenseToIncrement.inherentValue += action.payload.value;
+        increaseBonusXX(defenseToIncrement, action.payload.bonusType, action.payload.value);
+        // defenseToIncrement.inherentValue += action.payload.value;
       }
+      recalculateTotalBonus(state);
       return { ...state,
         defenses: [...restOfDefenses, defenseToIncrement].sort((a, b) => a.name > b.name) };
     }
+
+    //------------------------------------------------------------------------------
+    case ACTION_TYPES.CALCULATE_BONUS:
+      recalculateTotalBonus(state);
+      return { ...state };
+
+    //------------------------------------------------------------------------------
     default:
       return state;
   }
