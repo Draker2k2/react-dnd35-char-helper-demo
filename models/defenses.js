4ffe186bde415b286c027075329bdf491b9e5c30
Draker2k2 <33083335+Draker2k2@users.noreply.github.com>
2017-10-30 13:17:56 +0100

    fixing defenses model

--- models/defenses.js
+++ models/defenses.js
@@ -3,9 +3,28 @@ const mongoose = require('mongoose');
 const defensesSchema = new mongoose.Schema({
   id: String,
   name: String,
+  bonuses: {
+    type: Array,
+    default: [
+      { type: 'bonus', value: 0 },
+      { type: 'shield', value: 0 },
+      { type: 'dodge', value: 0 },
+      { type: 'enhancement', value: 0 },
+      { type: 'inherit', value: 0 },
+      { type: 'racial', value: 0 },
+      { type: 'deflection', value: 0 },
+      { type: 'armor', value: 0 },
+      { type: 'naturalArmor', value: 0 },
+      { type: 'competence', value: 0 },
+      { type: 'insight', value: 0 },
+      { type: 'resistance', value: 0 },
+      { type: 'sacred', value: 0 },
+      { type: 'profane', value: 0 },
+      { type: 'divine', value: 0 },
+    ],
+  },
   initialValue: { type: Number, default: 0 },
-  inherentValue: { type: Number, default: 0 },
-  enhancementValue: { type: Number, default: 0 },
+  totalBonus: { type: Number, default: 0 },
 });
 
 module.exports = mongoose.model('defensesSchema', defensesSchema, 'defensesSchema');
